2a7c6ad8be119900408a4e5d89a5b9a5f5d9c208
mabrosimov <maksym.abrosimov@macys.com>
2022-06-03 09:42:32 +0300

    rename app class

--- src/main/java/com/task/Application.java
+++ src/main/java/com/task/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCassandraApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCassandraApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
